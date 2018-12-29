f54f5a7660086f40b1cab646f2b184af235c73c4
lvjie <Json.lv@eisoo.com>
2018-12-29 14:50:36 +0800

    第一次修改不用husky

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": 2
+    }
+};
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const getSum = (arr) => {
     let sum = 0;
     arr.forEach(value => {
         sum += value;
-    })
+    });
 
     return sum;
-}
-const arr = [1, 2, 3, 4]
+};
+const arr = [1, 2, 3, 4];
 const arrSum = getSum(arr);
-console.log(arrSum);
+console.log('结果为' + arrSum);
