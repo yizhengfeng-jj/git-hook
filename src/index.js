c3beeaab8a022d1ba46b3164fd28ac0fc49a2e5c
lvjie <Json.lv@eisoo.com>
2018-12-29 14:04:02 +0800

    提交git-hook

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// 设置一个求数字和的函数
+const getSum = (arr) => {
+    let sum = 0;
+    arr.forEach(value => {
+        sum += value;
+    })
+
+    return sum;
+}
+const arr = [1, 2, 3, 4]
+const arrSum = getSum(arr);
+console.log(arrSum);
