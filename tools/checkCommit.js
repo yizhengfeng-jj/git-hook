08a68efbe79b4fbc6b10861d2bcd323269d78b50
lvjie <Json.lv@eisoo.com>
2019-01-01 16:46:37 +0800

    AR-454 机器学习

--- tools/checkCommit.js
+++ tools/checkCommit.js
@@ -4,7 +4,12 @@ const fs = require('fs'); // 改模块用于读取文件;
 // 读取数据
 const data = fs.readFileSync('../.git/COMMIT_EDITMSG', 'utf-8');
 
-
+if (/^(AR|BIG)-\d+/.test(data)) {
+    console.log('符合要求');
+}else {
+    console.log('不符合要求');
+}
+console.log(process);
 console.log(data);
 console.log(data.indexOf(6));
 console.log('===============');
