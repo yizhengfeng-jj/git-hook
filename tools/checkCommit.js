38ff3f2dd12935b0e498237866c1c0f48ab00349
lvjie <Json.lv@eisoo.com>
2019-01-01 17:06:35 +0800

    SPECIAL-COMMIT 现场问题

--- tools/checkCommit.js
+++ tools/checkCommit.js
@@ -4,14 +4,25 @@ const fs = require('fs'); // 改模块用于读取文件;
 // 读取数据
 const data = fs.readFileSync('../.git/COMMIT_EDITMSG', 'utf-8');
 
+// 打印信息
+const info = `
+    提交信息不符合规范，请按照以下规范提交信息:
+        需求提交：AR-xxx xx(AR-456 机器学习)
+        BUG提交：BIG-xxx xx(BIG-456 agent分页缺失)
+        特殊提交：SPECIAL-COMMIT xx(SPECIAL-COMMIT 产品现场问题)
+`;
+
+// 特殊的提交，直接方形
+if (/^SPECIAL-COMMIT/.test(data)) {
+    process.exit(0);
+}
+
+console.log('aaaaaa');
 if (/^(AR|BIG)-\d+/.test(data)) {
     console.log('符合要求');
     process.exit(0);
-}else {
-    console.log('不符合要求');
-    process.exit(1);
 }
-console.log(process);
-console.log(data);
-console.log(data.indexOf(6));
-console.log('===============');
+
+console.error(info);
+process.exit(1);
+
