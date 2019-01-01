4b12990c29732662328e7082e7c3b2c02be03cad
lvjie <Json.lv@eisoo.com>
2019-01-01 16:19:52 +0800

    556

--- tools/checkCommit.js
+++ tools/checkCommit.js
@@ -2,7 +2,7 @@
 const fs = require('fs'); // 改模块用于读取文件;
 
 // 读取数据
-const data = fs.readFileSync('../.git/COMMIT_EDITMSG');
+const data = fs.readFileSync('../.git/COMMIT_EDITMSG', 'utf-8');
 
 
 console.log(data);
